103e47d9ab63142b64530ccce34888b7360b1b62
Delvecchi <heitordelveccho@hotmail.com>
2023-08-03 15:14:14 -0300

    Finalização do exercicio de calculo de idade

--- script.js
+++ script.js
@@ -9,33 +9,117 @@ function calcular(event) {
     let usuario = pegarValores()
 
     // Passo2
-    let segundos = calcularSegundos(usuario.nascimento, usuario.data)
+    // let segundos = calcularSegundos(usuario.nascimento)
+
+    usuario = dadosAtualizado(usuario)
+
+    cadastrarIdade(usuario)
+    
+    carregarUsuarios()
+
+
+    
 }
+
 function pegarValores() {
+    let nomeRecebido = document.getElementById("nome").value.trim()
     let valoresCaprutado = Date.parse(document.getElementById("data").value);
-    
-
-    let dataAtual = Date.parse(new Date()) 
-     
-
-    let dadosIdade= {
-        nascimento: valoresCaprutado,
-        data: dataAtual 
-    }
-    console.log(dadosIdade)
-    
-
-    return dadosIdade
-}
-
-function calcularSegundos(nascimento, data) {
-    let segundosCalculados = data - nascimento
+    let atual = Date.parse(new Date()) 
+    let segundosCalculados = atual  - valoresCaprutado
 
     segundosCalculados = segundosCalculados / 3600
     segundosCalculados = segundosCalculados / 8760
-    segundosCalculados = segundosCalculados * 0,0001
+    segundosCalculados = parseInt(segundosCalculados * 0.001)
 
-    console.log(segundosCalculados)
+    let dadosUsuario = {
+        nome: nomeRecebido,
+        nascimento: valoresCaprutado,
+        idade: segundosCalculados
+         
+    }
+    console.log(dadosUsuario)
+    
 
-    return segundosCalculados
+    return dadosUsuario
+}
+
+function dadosAtualizado(dadosUsuario) {
+    
+    let dataHoraAtual = new Intl.DateTimeFormat('pt-BR', { timeStyle: 'long', dateStyle: 'short' }).format(Date.now())
+
+    console.log(dataHoraAtual);
+
+    // Organizando o objeto para salvar
+    let dadosUsuarioAtualizado = {
+        ...dadosUsuario,
+        dataCadastro: dataHoraAtual
+    }
+    console.log(dadosUsuarioAtualizado)
+
+    return dadosUsuarioAtualizado;
+}
+
+
+function cadastrarIdade(dadosUsuario) {
+    let listaUsuarios = []
+
+    // Se houver uma lista de usuarios no localStorage, carregar isso para a variavel listaUsuarios
+    if (localStorage.getItem("usuariosCadastrados") != null) {
+        listaUsuarios = JSON.parse( localStorage.getItem("usuariosCadastrados") )
+    }
+
+    // Adiciona o usuario na lista de usuarios
+    listaUsuarios.push(dadosUsuario)
+
+    // Salva a listaUsuarios no localStorage
+    localStorage.setItem("usuariosCadastrados",  JSON.stringify(listaUsuarios) )
+}
+
+function carregarUsuarios() {
+    let listaCarregada = []
+
+    if ( localStorage.getItem("usuariosCadastrados") != null ) {
+        listaCarregada = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
+
+    if(listaCarregada.length == 0) {
+        // Se não tiver nenhum usuario cadastrado, mostrar mensagem
+        let tabela = document.getElementById("corpo-tabela")
+
+        tabela.innerHTML = `<tr class="linha-mensagem">
+            <td colspan="6">Nenhum usuario cadastrado ☹ </td>
+        </tr>`
+
+    } else {
+        // Montar conteudo da tabela
+        montarTabela(listaCarregada)
+    }
+
+    console.log(listaCarregada)
+}
+
+window.addEventListener("DOMContentLoaded", () => carregarUsuarios() )
+
+function montarTabela(listaUsuarios) {
+    let tabela = document.getElementById("corpo-tabela")
+
+    let template = ""
+
+    listaUsuarios.forEach(usuario => {
+        template += `<tr>
+            <td data-cell="idade">${usuario.nome}</td>
+            <td data-cell="data de cadastro">${usuario.idade}</td>
+            <td data-cell="data de cadastro">${usuario.dataCadastro}</td>
+        </tr>`
+    })
+
+    tabela.innerHTML = template;
+}
+
+function deletarRegistros() {
+    // Remove o item do localStorage
+    localStorage.removeItem("usuariosCadastrados")
+
+    // Recarrega a página
+    window.location.reload()
 }
